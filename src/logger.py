5d25cd381a2c62d9a474764af7f25dae2b16cfd9
Neil Nazareth <neilnazareth@uni-koblenz.de>
2025-06-06 15:03:50 +0200

    logging and exception handling

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)  # ✅ Correct usage
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
