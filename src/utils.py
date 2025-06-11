b219253d16d9722837add855934719267b94af9e
neil-nazareth <neil.nazareth@gmail.com>
2025-06-11 10:42:40 +0200

    Added Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -25,7 +26,7 @@ def save_object(file_path, obj):
         print(f"Error saving object: {e}")
         raise e
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     """
     Evaluate multiple regression models and return their R2 scores.
     
@@ -41,14 +42,30 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     """
     try:
         report = {}
-        for model_name, model_instance in models.items():
-            model_instance.fit(X_train, y_train)
-            y_train_pred = model_instance.predict(X_train)
-            y_test_pred = model_instance.predict(X_test)
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            report[model_name] = test_model_score
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
